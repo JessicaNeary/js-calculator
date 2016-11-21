0d0ff06b3704911111605e3735bcc604cd6c2e9e
Jess Neary <Caneary@hotmail.com>
2016-11-21 14:00:59 +1300

    added comments and refractored code

--- calculator.js
+++ calculator.js
@@ -1,55 +1,43 @@
+//hold the current unsolved calculation
 var stream = '';
+//hold the result of the last calculation
 var result = 0;
 
-function calculate(){
-  result = eval(stream);
-  display(result);
-	stream = '';
-}
-
-function decimal(){
-	stream += '.';
-  display(stream);
-}
-
-function c(){
-  stream = "";
-  display(stream);
-}
-
-function number(a){
-  stream += ''+a;
-  display(stream);
-}
-
-var times = function(){
-	checkStream();
-  stream += '*';
-  display(stream);
-}
-
-var divide = function(){
-	checkStream();
-  stream += '/';
-  display(stream);
-}
-
-var add = function(){
-	checkStream();
-  stream += '+';
-  display(stream);
-}
-
-var subtract = function(){
-	checkStream();
-  stream += '-';
+//Runs specified function and updates display
+function x(y){
+	y;
 	display(stream);
 }
 
+//Adds given operation to the stream
+function operat(x){
+	checkStream();
+	add(x);
+}
+
+//Adds argument to the stream
+function add(a){
+  stream += a;
+}
+
+//clears stream
+function c(){
+  stream = "";
+}
+
+//calculates and displays result of calculation
+function calculate(){
+	result = eval(stream);
+	display(result);
+	c();
+}
+
+//updates the display
 function display(x){
 	document.getElementById('display').innerHTML = x.toString().substr(0,10);
 }
 
+//when an operation is pressed, checks if there are any numbers in the stream, otherwise uses latest result
 function checkStream(){
 	if(stream === ''){
 		stream = result;
